e0887929da72d09408be8da6de0fe0863e5391e3
Albert Dahlin <info@albertdahlin.com>
2018-07-23 20:42:08 +0000

    Moved style definitions to top level

--- src/Main.elm
+++ src/Main.elm
@@ -373,33 +373,52 @@ accordionHtmlInline openMsg isOpen acc =
 accordionSE : msg -> Bool -> Accordion -> Element msg
 accordionSE openMsg isOpen acc =
     Element.column
-        [ Element.width Element.fill
-        , Element.spacing 12
-        ]
+        accordionWrapperStyle
         [ Element.paragraph
-            [ Element.onClick openMsg
-            , Element.pointer
-            , Element.padding 8
-            , Font.size 20
-            , Font.color (Color.black)
-            , Font.family [ Font.typeface "Arial", Font.sansSerif ]
-            , Bg.color (Color.rgb 0xEE 0xEE 0xEE)
-            , Element.width Element.fill
-            , Border.color (Color.rgb 0xAA 0xAA 0xAA)
-            , Border.solid
-            , Border.width 1
-            , Region.heading 4
-            ]
+            ( Element.onClick openMsg
+            :: accordionHeadingStyle
+            )
             [ Element.text acc.heading
             ]
         , Element.paragraph
-            [ if isOpen then Element.height Element.shrink else Element.height (Element.px 0)
-            , Element.clip
-            , Element.width Element.fill
-            , Font.family [ Font.typeface "Arial", Font.sansSerif ]
-            , Font.size 16
-            , Font.color (Color.black)
-            ]
+            ( if isOpen then
+                (Element.height Element.shrink) :: accordionContentStyle
+              else
+                (Element.height (Element.px 0)) :: accordionContentStyle
+            )
             [ Element.text acc.content
             ]
         ]
+
+
+accordionHeadingStyle : List (Element.Attribute msg)
+accordionHeadingStyle =
+    [ Element.pointer
+    , Element.padding 8
+    , Font.size 20
+    , Font.color (Color.black)
+    , Font.family [ Font.typeface "Arial", Font.sansSerif ]
+    , Bg.color (Color.rgb 0xEE 0xEE 0xEE)
+    , Element.width Element.fill
+    , Border.color (Color.rgb 0xAA 0xAA 0xAA)
+    , Border.solid
+    , Border.width 1
+    , Region.heading 4
+    ]
+
+
+accordionContentStyle : List (Element.Attribute msg)
+accordionContentStyle =
+    [ Element.clip
+    , Element.width Element.fill
+    , Font.family [ Font.typeface "Arial", Font.sansSerif ]
+    , Font.size 16
+    , Font.color (Color.black)
+    ]
+
+
+accordionWrapperStyle : List (Element.Attribute msg)
+accordionWrapperStyle =
+    [ Element.width Element.fill
+    , Element.spacing 12
+    ]
